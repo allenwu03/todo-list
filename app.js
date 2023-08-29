46b29da201b587a23933911b22324a7d74e96136
Allen <allenwu032016@gmail.com>
2023-08-29 23:04:30 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose') //載入mongoose
+
+// 僅在非正式環境時(非Production正式機)，使用dotenv
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+}
 
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology:true}) //設定連線到mongoDB
+
+const db = mongoose.connection
+
+db.on('error',() =>{
+    console.log('mongodb error')
+})
+
+db.once('open',() =>{
+    console.log('mongodb connected!')
+})
 
 app.get('/',(req,res) =>{
     res.send('hello world!')
