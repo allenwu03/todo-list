41b6b19e177c3fa39d96c7532e92996c3233128c
Allen <allenwu032016@gmail.com>
2023-08-30 23:39:19 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ if(process.env.NODE_ENV != 'production'){
     require('dotenv').config()
 }
 
+const Todo = require('./models/todo') //載入 Todo model
+
 const app = express()
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology:true}) //設定連線到mongoDB
 
@@ -26,7 +28,10 @@ app.set('view engine','hbs')
 //設定路由
 //Todo首頁
 app.get('/',(req,res) =>{
-    res.render('index')
+    Todo.find() // 取出Todo model裡的所有資料
+        .lean() // 把Mongoose的Model物件轉換成乾淨的JavaScript資料陣列
+        .then(todos => res.render('index',{todos}))// 將資料傳給index樣板
+        .catch(error => console.error(error))// 錯誤處理
 })
 
 app.listen(3000, () =>{
